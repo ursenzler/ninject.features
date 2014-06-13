d35579145dc345ca5a13d0c6130c99a6ec743eef
Philipp Dolder <philipp.dolder@gmail.com>
2014-06-13 09:27:45 +0200

    Another change

--- source/Ninject.FeatureDumper/FeatureProcessor.cs
+++ source/Ninject.FeatureDumper/FeatureProcessor.cs
@@ -29,6 +29,7 @@ namespace Ninject.FeatureDumper
     public class FeatureProcessor
     {
         private readonly HashSet<Tuple<Type, Type>> chain = new HashSet<Tuple<Type, Type>>();
+
         private readonly HashSet<Type> allFeatures = new HashSet<Type>();
 
         public Features ProcessAssemblies(IEnumerable<Assembly> assemblies)
